216ac6c27b4e75ff4549283447804f2a8bb3f8dc
Apoorv Shrivastava <apoorv.shrivastava@lilly.com>
2023-08-11 12:45:34 +0530

    message

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    number=26
+    name=[1,2]
+    return render_template("index.html",num=number,name=name)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
